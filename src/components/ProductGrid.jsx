e79aea784d1341dd51ba418172343a8a42aa9b71
chaitu328 <konakallachaitanya@gmail.com>
2025-04-13 19:19:50 +0530

    done

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -8,21 +8,21 @@ export const ProductGrid = ({ viewMode }) => {
         id: 1, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/eco-friendly-grey-backpack.jpg',
+        image: '/assets/eco-friendly-grey-backpack.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
       { 
         id: 2, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: 'OUT OF STOCK', 
-        image: '/src/assets/sustainable-plush-toy.jpg',
+        image: '/assets/sustainable-plush-toy.jpg',
         altText: 'Colorful sustainable plush toy made from recycled materials'
       },
       { 
         id: 3, 
         name: 'PRODUCT NAME', 
         status: '', 
-        image: '/src/assets/handcrafted-leather-strap.jpg',
+        image: '/assets/handcrafted-leather-strap.jpg',
         altText: 'Handcrafted leather strap accessory with metal buckle'
       },
       // Only rendering a few products to minimize DOM size
@@ -30,56 +30,56 @@ export const ProductGrid = ({ viewMode }) => {
         id: 4, 
         name: 'PRODUCT NAME', 
         status: '', 
-        image: '/src/assets/organic-cotton-cap.jpg',
+        image: '/assets/organic-cotton-cap.jpg',
         altText: 'White organic cotton cap with adjustable strap'
       },
       { 
         id: 5, 
         name: 'PRODUCT NAME', 
         status: '', 
-        image: '/src/assets/dark-grey-backpack.jpg',
+        image: '/assets/dark-grey-backpack.jpg',
         altText: 'Dark grey recycled material backpack with side pockets'
       },
       { 
         id: 6, 
         name: 'PRODUCT NAME', 
         status: '', 
-        image: '/src/assets/recycled-plush-dinosaur.jpg',
+        image: '/assets/recycled-plush-dinosaur.jpg',
         altText: 'Yellow recycled fabric plush dinosaur toy'
       },
       { 
         id: 7, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/7.jpg',
+        image: '/assets/7.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
       { 
         id: 8, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/8.jpg',
+        image: '/assets/8.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
       { 
         id: 9, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/9.jpg',
+        image: '/assets/9.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
       { 
         id: 10, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/10.jpg',
+        image: '/assets/10.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
       { 
         id: 11, 
         name: 'PPPXC MLKYWAY DRESS III', 
         status: '', 
-        image: '/src/assets/11.jpg',
+        image: '/assets/11.jpg',
         altText: 'Eco-friendly grey recycled backpack with foldable top'
       },
     ];
